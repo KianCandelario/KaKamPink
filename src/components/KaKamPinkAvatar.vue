e5a7a67406b0d793827fcead09a18d3fe3081838
ajpanoy14 <panoy_angelojames@plpasig.edu.ph>
2024-05-22 00:42:19 +0800

    added avatar and checkbox

---
+++ src/components/KaKamPinkAvatar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div :class="avatarClass" :style="avatarStyle">
+      <img :src="src" :alt="alt" v-if="src" class="w-full h-full object-cover rounded-full" />
+      <div v-else class="w-full h-full flex items-center justify-center bg-gray-200 text-gray-500 rounded-full">
+        {{ initials }}
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'kakampink-avatar',
+    props: {
+      src: {
+        type: String,
+        default: null,
+      },
+      alt: {
+        type: String,
+        default: 'kakampink-avatar',
+      },
+      size: {
+        type: String,
+        default: '16', // Default size is 16 (which means 4rem or 64px in Tailwind)
+      },
+      name: {
+        type: String,
+        default: '',
+      },
+    },
+    computed: {
+      avatarClass() {
+        return `w-${this.size} h-${this.size} rounded-full overflow-hidden`;
+      },
+      avatarStyle() {
+        return {
+          width: `${this.size * 4}px`,
+          height: `${this.size * 4}px`,
+        };
+      },
+      initials() {
+        if (!this.name) return '';
+        return this.name.split(' ').map(part => part[0]).join('').toUpperCase();
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
---
+++ src/components/KaKamPinkCheckbox.vue
@@ -0,0 +1,39 @@
+<template>
+    <label :class="['flex items-center cursor-pointer', className]">
+      <input 
+        type="checkbox" 
+        :checked="modelValue" 
+        @change="$emit('update:modelValue', $event.target.checked)" 
+        :class="['form-checkbox', checkboxClass]" 
+      />
+      <span class="ml-2">{{ label }}</span>
+    </label>
+  </template>
+  
+  <script>
+  export default {
+    name: 'kakampink-checkbox',
+    props: {
+      modelValue: {
+        type: Boolean,
+        default: false,
+      },
+      label: {
+        type: String,
+        default: '',
+      },
+      className: {
+        type: String,
+        default: '',
+      },
+      checkboxClass: {
+        type: String,
+        default: '',
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* You can add custom styles here if needed */
+  </style>
