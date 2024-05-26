daaa90ab58c4263aa4dbdd12d046bfd98738ded9
Russel Custodio <russelcustodio49@gmail.com>
2024-05-26 15:46:56 +0800

    updated: header

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,41 +1,55 @@
 <template>
-    <div class = "max-w flex flex-col justify-items-center items-center font-[poppins] font-light-bold absolute bottom-0 ">
-        <div class = "bg-transparent">
-            <img class = " h-[10rem] bg-transparent" src="/src/assets/footer/logo.svg" alt="">
+<div class = "w-full bg-pink-shade-three p-3">
+    
+        <div class = " bg-transparent flex flex-row gap-5 justify-center items-center p-5">
+            <!-- line -->
+            <div class = "border-2 border-black w-[30%]"></div>
+            <div class = "bg-transparent">
+                <ul class = " bg-transparent flex flex-row gap-5 justify-center items-center font-poppins">
+                    <li class = "bg-transparent" ><img class = "bg-transparent" src="/src/assets/footer/fb.svg" alt=""></li>
+                    <li class = "bg-transparent"><img class = "bg-transparent"  src="/src/assets/footer/github.svg" alt=""></li>
+                    <li class = "bg-transparent"><img class = "bg-transparent" src="/src/assets/footer/github.svg" alt=""></li>
+                    <li class = "bg-transparent"><img class = "bg-transparent" src="/src/assets/footer/fb.svg" alt=""></li>
+                </ul>
+            </div>
+            <div class = "border-2 border-black w-[30%]"></div>
         </div>
 
-        <div class = "flex flex-row justify-items-center items-center gap-5 pb-5 ">
-            <a
-            v-for = "(social, index) in socialmedia" :key = "index"
-            >
-                <img class = "max-w-[50px] " :src="social.src" alt="">
-            </a>
-        </div>
-
-        <div class = "bg-transparent w-[80%] text-center">
-            <p class = "" > KAKAMPINK KAKAMPINK KAKAMPINK KAKAMPINK KAKAMPINK KAKAMPINK KAKAMPINK KAKAMPINK KAKAMPINK Mananatiling kakampink, sa puso't isipan KAKAMPINK KAKAMPINK KAKAMPINK KAKAMPINK </p>
-        </div>
-
-        <div class = "border-b-2 border-black w-[50%] p-2">
-
-        </div>
-
-        <div class = "p-5">
-            <h1> © 2024 Copyright: Kakampink.com</h1>
+    <div class = "bg-transparent">
+        <div class = "bg-transparent font-poppins flex flex-col justify-center items-center text-center">
+            <img class = "bg-transparent w-[200px] h-[200px]" src="/src/assets/footer/kkp-logo.svg" alt="">
+            <p class = " bg-transparent text-[25px] w-[40%]">{{ content }}</p>
+            <p class = " bg-transparent text-[20px] mt-5">Copyright © {{ year }} {{ companyname }}</p>
         </div>
     </div>
+
+    <div class = "bg-transparent mb-3">
+        <ul class = " bg-transparent flex flex-row gap-10 font-poppins text-[20px] items-center justify-center mt-10 underline">
+            <li v-for="(item, index) in nav" :key="index" class = "bg-transparent "> {{ item }} </li>
+            
+        </ul>
+    </div>
+    <div class = "flex justify-center bg-transparent p-5">
+        <div class = "border-2 border-black w-[75%] text-"></div>
+    </div>
+
+</div>
+    
+
+
 </template>
 
 
 <script>
     export default{
-        props: ['year', 'companyname', 'theme', 'number'],
+        props: ['year', 'companyname', 'theme', 'number', 'content'],
         data(){
             return{
                 socialmedia:[
                     { src: '/src/assets/footer/github.svg' },
                     { src: '/src/assets/footer/fb.svg' },
-            ]
+            ],
+            nav: ['Home', 'Contributors', 'Documentation'],
             }
         }
     }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="w-full flex flex-col md:flex-row justify-center items-center p-7 shadow-lg">
+    <div class="w-full flex flex-col md:flex-row justify-center items-center p-7 shadow-lg sticky">
     <div class="w-full md:w-1/3 ml-0 mb-4 md:mb-0 flex justify-center md:justify-start">
         <img src="/src/assets/header/kkp-logo.svg" alt="KKP Logo" class="w-[90px] md:w-[130px]">
     </div>
 
     <div class="w-full md:w-1/2 flex justify-center md:justify-start">
     
-    <ul class="w-full flex flex-col md:flex-row gap-7 md:gap-16 items-center  text-base md:text-[23px] justify-end font-poppins">
+    <ul class="w-full flex flex-col md:flex-row gap-7 md:gap-14 items-center text-base md:text-[17px] justify-end font-poppins">
         <li  v-for="(item, index) in nav" :key="index">
-        <a href="#" class="hover:underline hover:text-pink-500">{{ item }}</a>
+            <router-link active-class = "underline text-pink-500" :to = "item.link" >{{ item.name }}</router-link>
       </li>
-    <li><a href="#"><img class="w-3 h-3 md:w-7 md:h-7" src="/src/assets/header/gitblack.png" alt="GitHub"></a></li>
+    <li><a href="https://github.com/KianCandelario/KaKamPink"><img class="w-3 h-3 md:w-7 md:h-7" src="/src/assets/header/gitblack.png" alt="GitHub"></a></li>
 </ul>
 
     </div>
@@ -19,12 +19,17 @@
 </template>
 
 <script>
-export default{
-    data(){
-            return{
-                nav: ['Home', 'Contributors', 'Documentation']
-            }
-            
-        }
-}
+export default {
+  data() {
+    return {
+      nav: [
+        { name: 'Home', link: '/' },
+        { name: 'Contributors', link: '/contributors' },
+        { name: 'Documentation', link: '/documentation' }
+      ],
+      currentItem: null
+    };
+  },
+};
+
 </script>
