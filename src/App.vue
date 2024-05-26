f8f950931db41eef1f0f9e2da469f21b5610db55
Mel <melzedric04@gmail.com>
2024-05-26 22:19:02 +0800

    modified App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
+import Footers from './components/Footer.vue'
 import HelloWorld from './components/HelloWorld.vue'
+import Sheet_One from './components/Sheet_One.vue'
+import Sheet_Two from './components/Sheet_Two.vue'
+import Sheet_Three from './components/Sheet_Three.vue'
+import BottomSheets_One from './components/BottomSheets_One.vue'
+import BottomSheet_Two from './components/BottomSheet_Two.vue'
+import BottomSheet_Three from './components/BottomSheet_Three.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <BottomSheet_Three/>
 </template>
