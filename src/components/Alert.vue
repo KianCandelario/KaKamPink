aaadf166a82bb396936ed917ffcee6ed496b783a
EmmanEjudo <ejudoemmanuel@gmail.com>
2024-05-31 21:08:27 +0800

    Template for Alert

--- src/components/Alert.vue
+++
@@ -1,6 +0,0 @@
-
-<template>
-    <v-alert closable title="V-Alert" text="This is v-alert, the alert component of vue."></v-alert>
-</template>
-
-<script> </script>
---
+++ src/components/kkp_Alert.vue
@@ -0,0 +1,25 @@
+
+<template>
+    <div class=" bg-gray-400 space-y-2 p-4 rounded-lg w-80">
+        <div class=" flex-col space-x-2">
+            <div class=" flex shrink-0 space-x-2">
+                <img class=" h-6 w-6" src="/src/assets/alert/information-button.png" alt="">
+                <h2 class=" font-semibold text-lg">Alert</h2>
+            </div>
+            <div class=" flex-1 text-left">
+                <p class="">This is an Alert, ceated by our team - KaKamPink.</p>
+            </div>
+        </div>
+        <div class=" grid grid-cols-3 grid-rows-1">
+            <span></span>
+            <span></span>
+            <button class=" bg-gray-600 py-1">close</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+
+</script>
