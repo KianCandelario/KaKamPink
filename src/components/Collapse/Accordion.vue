74afa3da2ea745e164e21c1d402c8c48a27eed02
Kian Candelario <kiancandelario1@gmail.com>
2024-06-07 23:31:34 +0800

    feat: Added rough draft of collapse components

---
+++ src/components/Collapse/Accordion.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full my-4">
+    <div @click="toggle" class="cursor-pointer bg-pink-shade-four p-4 rounded-t-lg font-kumbh-sans">
+      <slot name="header"></slot>
+    </div>
+    <div v-if="isOpen" class="p-4 bg-pink-shade-two font-poppins rounded-b-lg">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccordionCollapse',
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+    }
+  }
+}
+</script>
