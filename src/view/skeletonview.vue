19cc26959d2097d8cbe0b687c38b2aa2ca7abae1
EmmanEjudo <ejudoemmanuel@gmail.com>
2024-06-09 23:01:08 +0800

    Trying loader

---
+++ src/view/skeletonview.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class = "bg-pink-shade-two w-full p-3 overflow-scroll overflow-x-hidden h-[100%]">
+        <div class="flex flex-col justify-center items-center rounded-xl">
+            <div class = "w-[80%]">
+    
+                <!-- Component Header -->
+                <h1 class="bg-transparent text-start text-[45px] font-poppins mt-5 text-pink-shade-four font-bold tracking-wider">Component Title</h1>
+                <p class="font-poppins mt-5 mb-5 text-[18px]">Component Description</p>
+    
+                <!-- Component sub -->
+                    <div>
+                        <div class="w-[80%]">
+                        <h1 class="bg-transparent text-start text-[30px] font-poppins mt-3"> Sub-Container  </h1>
+                        <p class="font-poppins mt-2 text-[15px]"> Sub-Container Description </p>
+                    </div>
+            
+                    <div class="w-[100%] h-[20vh] bg-white flex items-center justify-center rounded-2xl m-5">
+                        Component area
+                    </div>
+                </div>
+            
+    
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
