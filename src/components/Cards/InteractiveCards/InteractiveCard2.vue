2363bc5dbf03f641e5cd681efe3c2601825063f9
Kian Candelario <kiancandelario1@gmail.com>
2024-06-09 17:53:08 +0800

    feat and chore: Added new components and moved files to separate folders

--- src/components/Cards/InteractiveCards/InteractiveCard2.vue
+++ src/components/Cards/InteractiveCards/InteractiveCard2.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-[27%]  my-6 bg-pink-shade-three rounded-lg shadow-lg overflow-hidden transform transition-transform hover:scale-105">
+  <div class="w-[27%]  my-6 bg-pink-shade-three rounded-lg shadow-lg overflow-hidden transform transition-transform hover:translate-x-2 border-2 border-pink-shade-two hover:border-none">
     <img class="w-full h-48 object-cover" src="https://via.placeholder.com/150" alt="Card Image">
     <div class="px-6 py-4">
-      <h2 class="font-kumbh-sans text-2xl mb-2 text-pink-shade-four">Card Title</h2>
+      <h2 class="font-kumbh-sans font-bold text-2xl mb-3 text-pink-shade-four border-b-2 border-pink-shade-two">Card Title</h2>
       <p class="font-quicksand text-pink-shade-four text-base">
         This card has an interactive hover effect.
       </p>
@@ -15,7 +15,7 @@
 
 <script>
 export default {
-  name: 'InteractiveCard'
+  name: 'InteractiveCard2'
 }
 </script>
 
