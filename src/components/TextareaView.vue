22199db5467c0619afb9d554ec6a5d33cd4671ba
Kian Candelario <kiancandelario1@gmail.com>
2024-06-09 19:45:20 +0800

    feat: Added view for textarea components

--- src/components/TextareaView.vue
+++ src/components/TextareaView.vue
@@ -4,18 +4,40 @@
         <div class = "w-[80%]">
 
             <!-- Component Header -->
-            <h1 class="bg-transparent text-start text-[45px] font-poppins mt-5 text-pink-shade-four font-bold tracking-wider">Collapse Component</h1>
-            <p class="font-poppins mt-5 mb-5 text-[18px]"> The Collapse Component is designed to provide a user-friendly way to hide or reveal content sections on demand. It allows users to toggle between showing or hiding sections of content with a smooth animation, enhancing the user experience by decluttering the interface and focusing attention on relevant information. </p>
+            <h1 class="bg-transparent text-start text-[45px] font-poppins mt-5 text-pink-shade-four font-bold tracking-wider">Textarea Components</h1>
+            <p class="font-poppins mt-5 mb-5 text-[18px]">The Textarea component is a user interface element designed to allow users to input and edit multiple lines of text within a form or interface. It provides a larger input area compared to a standard text input field, accommodating longer text entries such as comments, descriptions, or messages.</p>
 
             <!-- Component sub -->
                 <div>
                     <div class="w-[80%]">
-                    <h1 class="bg-transparent text-start text-[30px] font-poppins mt-3"> Default Collapse </h1>
-                    <p class="font-poppins mt-2 text-[15px]"> A versatile UI element that simply allows users to hide or reveal content sections with a simple click or tap. </p>
+                    <h1 class="bg-transparent text-start text-[30px] font-poppins mt-3"> Default Textarea </h1>
+                    <p class="font-poppins mt-2 text-[15px]"> A simple and versatile multiline text input field, the Default Textarea Component allows users to enter and edit long-form text. Key features include resizable dimensions, optional placeholder text, and automatic scrollbars for overflow content. Ideal for capturing comments, messages, or descriptions within forms and interfaces. </p>
                 </div>
         
-                <div class="w-[100%] h-[40vh] bg-white flex items-center justify-center rounded-2xl m-5">
-                    <DefaultCollapse></DefaultCollapse>
+                <div class="w-[100%] h-[20vh] bg-white flex items-center justify-center rounded-2xl m-5">
+                    <TextareaDefault></TextareaDefault>
+                </div>
+
+                <div>
+                    <div class="w-[80%]">
+                    <h1 class="bg-transparent text-start text-[30px] font-poppins mt-3"> Textarea with Label </h1>
+                    <p class="font-poppins mt-2 text-[15px]"> A form element that combines a multiline text input area with an associated label, providing a clear and accessible way for users to input and understand the required text information. The label ensures the textarea is properly identified, enhancing usability and accessibility. </p>
+                    </div>
+                </div>
+        
+                <div class="w-[100%] h-[20vh] bg-white flex items-center justify-center rounded-2xl m-5">
+                    <TextareaWithLabel></TextareaWithLabel>
+                </div>
+
+                <div>
+                    <div class="w-[80%]">
+                    <h1 class="bg-transparent text-start text-[30px] font-poppins mt-3"> Textarea with Counter </h1>
+                    <p class="font-poppins mt-2 text-[15px]"> Allows users to input and edit multi-line text with real-time character count feedback. As users type, a counter displays the remaining characters. If the user exceeds the maximum character limit, the text color changes to red, providing a clear visual indication of the limit breach. </p>
+                    </div>
+                </div>
+        
+                <div class="w-[100%] h-[20vh] bg-white flex items-center justify-center rounded-2xl m-5">
+                    <TextareaWithCounter></TextareaWithCounter>
                 </div>
             </div>
         
@@ -26,15 +48,15 @@
 </template>
 
 <script>
-import DefaultCollapse from './Collapse/Default.vue';
-import AccordionCollapse from './Collapse/Accordion.vue';
-import HoverCollapse from './Collapse/Hover.vue';
+import TextareaDefault from './Textarea/Default.vue';
+import TextareaWithLabel from './Textarea/Label.vue';
+import TextareaWithCounter from './Textarea/Count.vue';
 
 export default {
   components: {
-    DefaultCollapse,
-    AccordionCollapse,
-    HoverCollapse
+    TextareaDefault,
+    TextareaWithLabel,
+    TextareaWithCounter
   }
 }
 </script>
