7e6b08dd7b53683906aaa58103e4537c2f3efef3
Russel Custodio <russelcustodio49@gmail.com>
2024-06-10 16:24:17 +0800

    modified: web icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
@@ -39,4 +40,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
